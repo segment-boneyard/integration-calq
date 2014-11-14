a40e9a43503d934efe5feb238cf3dbff32da3d39
Garrett Johnson <garrett@segment.io>
2014-11-14 08:40:24 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,129 @@
+
+var Test = require('segmentio-integration-tester');
+var helpers = require('./helpers');
+var facade = require('segmentio-facade');
+var mapper = require('../lib/mapper');
+var should = require('should');
+var assert = require('assert');
+var Calq = require('..');
+
+describe('Calq', function () {
+  var settings;
+  var calq;
+  var test;
+
+  beforeEach(function(){
+    settings = { writeKey: '0e116d3930b329831f146716c3667dfe' };
+    calq = new Calq(settings);
+    test = Test(calq, __dirname);
+  });
+
+  it('should have the correct settings', function(){
+    test
+      .name('Calq')
+      .channels(['server', 'client', 'mobile'])
+      .ensure('settings.writeKey')
+      .retries(3);
+  });
+
+  describe('.validate()', function () {
+    it('should be invalid when .writeKey is missing', function () {
+      delete settings.writeKey;
+      test.invalid({}, settings);
+    });
+
+    it('should be valid when settings are complete', function(){
+      test.valid({}, settings);
+    });
+  });
+
+  describe('mapper', function(){
+    describe('track', function(){
+      it('should map basic track', function(){
+        test.maps('track-basic', settings);
+      });
+
+      it('should map full track', function(){
+        test.maps('track-full', settings);
+      });
+    });
+
+    describe('alias', function(){
+      it('should map alias', function(){
+        test.maps('alias', settings);
+      });
+    });
+
+    describe('identify', function(){
+      it('should map basic identify', function(){
+        test.maps('identify-basic', settings);
+      });
+    });
+  });
+
+  describe('.track()', function () {
+    it('should be able to track correctly', function(done){
+      var json = test.fixture('track-full');
+      test
+        .set(settings)
+        .track(json.input)
+        .sends(json.output)
+        .expects(200, done);
+    });
+
+    it('should be able to track a bare call correctly', function(done){
+      var json = test.fixture('track-basic');
+      test
+        .set(settings)
+        .track(json.input)
+        .sends(json.output)
+        .expects(200, done);
+    });
+
+    it('should error on an invalid write key', function(done){
+      test
+        .set({ writeKey: 'bad-key' })
+        .track(helpers.track())
+        .expects(400)
+        .error('cannot POST /track (400)', done);
+    });
+  });
+
+  describe('.identify()', function () {
+    it('should be able to identify correctly', function(done){
+      var json = test.fixture('identify-basic');
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200, done);
+    });
+
+    it('should error on invalid write key', function(done){
+      var json = test.fixture('identify-basic');
+      test
+        .set({ writeKey: 'bad-key' })
+        .identify(json.input)
+        .expects(400)
+        .error('cannot POST /profile (400)', done);
+    });
+  });
+
+  describe('.alias()', function () {
+    it('should be able to alias properly', function(done){
+      var json = test.fixture('alias');
+      test
+        .alias(json.input)
+        .sends(json.output)
+        .expects(200, done);
+    });
+
+    it('should error on invalid write key', function(done){
+      var json = test.fixture('alias');
+      test
+        .set({ writeKey: 'bad-key' })
+        .alias(json.input)
+        .expects(400)
+        .error('cannot POST /transfer (400)', done);
+    });
+  });
+});
